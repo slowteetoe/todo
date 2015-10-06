2ae57f4364729677d7abb502bae58aeb540dc7ff
Steven Lotito <steven.lotito@alumni.pitt.edu>
2015-10-05 18:14:20 -0700

    Stubbed in basic mongo connection

--- server.go
+++ server.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"encoding/xml"
+	"github.com/gorilla/context"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 	"log"
 	"net/http"
 	"os"
@@ -11,6 +14,16 @@ import (
 type SmsResponse struct {
 	XMLName xml.Name
 	Message string `xml:"Sms"`
+}
+
+type TodoItem struct {
+	Title       string
+	CompletedAt *time.Time
+}
+
+type TodoList struct {
+	Name      string
+	TodoItems []TodoItem
 }
 
 func respond(w http.ResponseWriter, r *http.Request) {
@@ -25,11 +38,66 @@ func respond(w http.ResponseWriter, r *http.Request) {
 	w.Write(x)
 }
 
+func todoList(w http.ResponseWriter, r *http.Request) {
+	db := context.Get(r, "db").(*mgo.Session)
+	c := db.DB("todolist").C("todolists")
+	result := TodoList{}
+	err := c.Find(bson.M{"name": "Basic Todo"}).One(&result)
+	if err != nil {
+		log.Fatal(err)
+	}
+	x, err := xml.MarshalIndent(result, "", "  ")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/xml")
+	w.Write(x)
+}
+
+type Server struct {
+	dbsession *mgo.Session
+}
+
+func NewServer() (*Server, error) {
+	dbsession, err := mgo.Dial("localhost")
+	if err != nil {
+		return nil, err
+	}
+	return &Server{dbsession: dbsession}, nil
+}
+
+func (s *Server) Close() {
+	s.dbsession.Close()
+}
+
+func (s *Server) WithData(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		dbcopy := s.dbsession.Copy()
+		defer dbcopy.Close()
+		context.Set(r, "db", dbcopy)
+		fn(w, r)
+	}
+}
+
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 		log.Printf("$PORT was unset, defaulting to %v", port)
+	}
+	server, err := NewServer()
+
+	if err != nil {
+		panic(err)
+	}
+	defer server.Close()
+	c := server.dbsession.DB("todolist").C("todolists")
+	err = c.Insert(&TodoList{Name: "Basic Todo", TodoItems: []TodoItem{TodoItem{Title: "Eat Breakfast"}, TodoItem{Title: "Eat Lunch"}}})
+
+	if err != nil {
+		log.Fatal(err)
 	}
 	s := &http.Server{
 		Addr:         ":" + port,
@@ -37,6 +105,7 @@ func main() {
 		WriteTimeout: 10 * time.Second,
 	}
 	http.HandleFunc("/incoming", respond)
+	http.HandleFunc("/list", server.WithData(todoList))
 
 	log.Fatal(s.ListenAndServe())
 }
